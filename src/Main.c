502e4db5d81d75d1b5c5e2d9f1a4f9c0928531f3
smato <65234883+smato-fr@users.noreply.github.com>
2022-10-17 19:52:05 +0200

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -1,1 +1,19 @@
+//FICHIER PRINCIPAL DEMARRE A L'EXECUTION
 
+
+//gestion affichage en console
+#include<Renderer.c>
+
+//gestion de la lecture des ressources
+#include<FileManger.c>
+
+
+//foncton principale
+int main() {
+  
+  //chargement des fichiers
+  int nb_map;
+  int*** maps;
+  loadingFiles(&nb_map, &maps);
+  
+}
