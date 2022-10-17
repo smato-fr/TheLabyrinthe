05b05782a1ee623d4b94011b0bf0e3b6c48688f5
smato <nvalette0@gmail.com>
2022-10-17 20:44:10 +0200

    Update Renderer.c

---
+++ src/Renderer.c
@@ -0,0 +1,27 @@
+
+
+
+
+/*
+
+int* map -> pointeur vers un tableau 2D caractérisant la carte
+int xMin, yMin -> coordonnées x, y de la case la plus en haut à gauche du tableau à afficher
+int xMax, yMax -> coordonnées x, y de la case la plus basse à droite du tableau à afficher
+
+
+|y/x|  0 | 1 | 2 | 3 |
+______________________
+| 0 |    |   |   |   |
+______________________
+| 1 |    | min | x | x |
+______________________
+| 2 |    | x | x | x |
+______________________
+| 3 |    | x | x | max |
+______________________
+x-> case à afficher
+
+*/
+void display(int* map, int xMin, int yMin, int xMax, int yMax) {
+
+}
