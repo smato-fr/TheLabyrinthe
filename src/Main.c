e56ad39cb615a852f35ba93130043e3f8b139c2f
smato <nvalette0@gmail.com>
2022-10-22 17:14:22 +0200

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -3,6 +3,26 @@
 
 //IMPORTATIONS
 
+//Structure partie
+struct Game
+{
+	//constantes
+	int level = 1; //id du level à charger
+  	int nb_map; //nombre de carte
+  	int* size_maps; //tableau des tailles des cartes
+  	int** maps; //tous les tableaux 2D
+
+
+	//variables du jeu
+  	int x_spawn, y_spawn;
+  	int current_size; //taille de la carte actuelle
+  	int* current_map;//carte actuelle où se trouve le joueur
+  	int x_player, y_player; //position du joueur
+};
+
+//enumération des erreurs
+#include<Error.h>
+
 //énumération des types de commande utilisateur (en console)
 #include<Command.h>
 
@@ -17,40 +37,31 @@
 
 
 
+//instance de game
+const Game game;
+
 //FONCTION PRINCIPALE
 int main() {
   	//INITIALISATION
-	int level = 1; //id du level à charger
-
-
-  	//chargement des fichiers
-  	int nb_map; //nombre de carte
-  	int* size_maps; //tableau des tailles des cartes
-  	int** maps; //tous les tableaux 2D
+	
 
   	if (loadingFiles(level, &nb_map, &size_maps, &maps)) //chargement des fichier 
   		assert("erreur lors du chargement des fichiers"); //lance une exception
 
 
-  	//variables du jeu
-  	int x_spawn, y_spawn;
-  	int current_size; //taille de la carte actuelle
-  	int* current_map;//carte actuelle où se trouve le joueur
-  	int x_player, y_player; //position du joueur
-
   	//recherche du point de spawn du joueur
   	for (int m = 0; m < nb_map; m++) {
-  		current_map = maps[m];
-  		current_size = size_maps[m];
+  		game.current_map = maps[m];
+  		game.current_size = size_maps[m];
 	  	for (int i = 0; i < current_size; i++) {
 
 	  		for (int j = 0; j < current_size; j++) {
 
 	  			if (current_map[i*current_size+j] == START) {
-	  				x_spawn=i;
-	  				y_spawn=j;
-	  				x_player=x_spawn;
-	  				y_player=y_player;
+	  				game.x_spawn=i;
+	  				game.y_spawn=j;
+	  				game.x_player=game.x_spawn;
+	  				game.y_player=game.y_player;
 	  				break;
 	  			}
 
@@ -60,16 +71,47 @@ int main() {
   	}
 
 
-  	enum Command cmd;
-  	do {
+  	enum Command cmd = read_console();
+  	while(cmd != STOP) {
+  		
+  		(*Commands[cmd])();
+
 
   		//attente d'une instruction donnée par le joueur
   		cmd = read_console();
-
-  		
-  		
-
   	}
-  	while(cmd != STOP);
+  	
 
+}
+
+
+
+//fonctions appelées par  les commandes utilisateurs (définies dans Command.h)
+void up() {
+
+	if (game.x_player > 0) {
+
+
+
+	}
+
+	print(USER_ERROR_UNMOVABLE);
+}
+
+void down() {
+
+
+	print(USER_ERROR_UNMOVABLE);
+}
+
+void right() {
+
+
+	print(USER_ERROR_UNMOVABLE);
+}
+
+void left() {
+
+
+	print(USER_ERROR_UNMOVABLE);
 }
