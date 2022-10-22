876fc7c14aea228c1330e4252863bef036037b6d
smato <nvalette0@gmail.com>
2022-10-22 17:14:10 +0200

    Update Command.h

--- src/Command.h
+++ src/Command.h
@@ -1,10 +1,18 @@
 //énumérations de toutes les commandes utilisateurs
 enum Command {
-	STOP,
-	UP,
+	STOP=-1,
+	UP=0,
 	DOWN,
-	UP,
+	RIGHT,
 	LEFT,
 	
 
 }
+
+void stop();
+void run();
+void up();
+void down();
+void left();
+
+const void* Commands[]={&up, &down, &right, &left};
