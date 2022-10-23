50e54137974839dbdd6f6e127be8cc9e57cbc7df
Louis-Eliott <115985295+Louis-Eliott@users.noreply.github.com>
2022-10-23 16:37:57 +0200

    Update Interface.c

--- src/Interface.c
+++ src/Interface.c
@@ -6,6 +6,7 @@ Partie affichage et lecture en console
 */
 
 #include <stdio.h>
+#include <string.h>
 
 
 /*
@@ -37,8 +38,36 @@ void display(int* map, int xMin, int yMin, int xMax, int yMax) {
 demande à l'utilisateur d'entrer une commande sous une chaine de caractère
 renvoie la commande dans le type Command
 */
-enum Command read_console() {
-
+enum Command read_console() 
+{
+	
+	char mot[10];
+	/* demande a l'utilisateur d entrer un mot */
+	scanf("%s", mot);
+	/* test si le mot entre par l utilisateur correspond au mot associe a l action qui le fera aller en haut */
+	if (strcmp(mot, "up") == 0)
+	{
+		/* si c est le cas alors le personnage va en haut sinon il ne se passe rien*/
+		return (UP);
+	}
+	/* test si le mot entre par l utilisateur correspond au mot associe a l action qui le fera aller a gauche */
+	if (strcmp(mot, "left") == 0)
+	{
+		/* si c est le cas alors le personnage va a gauche sinon il ne se passe rien*/
+		return (LEFT);
+	}
+	/* test si le mot entre par l utilisateur correspond au mot associe a l action qui le fera aller en droite */
+	if (strcmp(mot, "right") == 0)
+	{
+		/* si c est le cas alors le personnage va a droite sinon il ne se passe rien*/
+		return (RIGHT);
+	}
+	/* test si le mot entre par l utilisateur correspond au mot associe a l action qui le fera aller en bas */
+	if (strcmp(mot, "down") == 0)
+	{
+		/* si c est le cas alors le personnage va en bas sinon il ne se passe rien*/
+		return (DOWN);
+	}
 }
 
 //affichage en console d'un string
