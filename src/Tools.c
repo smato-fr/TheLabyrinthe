f8a70be1cd14d38e25e2372e46473db1ec95c959
smato <nvalette0@gmail.com>
2022-10-28 11:09:52 +0200

    added numberOf method

--- src/Tools.c
+++ src/Tools.c
@@ -1,5 +1,17 @@
 
-//recherche de la position d'un élément dans la carte (renvoie les nb premier-s- trouvé-s-)
+//recherche le nombre d'apparatition d'un type de case dans la carte
+int numberOf(const int* map, const int size, const int type) {
+    int nb = 0;
+
+    for (int y = 0; y < size; y++)
+        for (int x = 0; x < size; x++)
+            if (map[y*size+x] == type)	  				
+                nb++;
+    
+    return nb;
+}
+
+//recherche des nb premières positions d'un type de case dans la carte
 int lookingFor(int* pos_x, int* pos_y, const int* map, const int size, const int type, int nb) {
     
     int i = 0; //nombre d'elements actuellement trouvés
